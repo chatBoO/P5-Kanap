04d3fbb7e11d1b2ee701846da8b16514024f8ed2
Mathieu Segaud <segaud.mathieu@gmail.com>
2022-11-21 13:53:13 +0100

    cart.js improved

--- front/js/cart.js
+++ front/js/cart.js
@@ -18,17 +18,25 @@ const saveTheBasket = (cartContent) => {
   };  
 
 
+/* 
+Fonction qui calcule la quantité et le prix total des articles. 
+A chaque itération de la première boucle on lance une deuxième boucle pour trouver une corresponsance d'ID entre les données de l'API et le panier en localStorage :
+Pour la quantité : Si correspondance alors on ajoute la quantité du produit à la valeur TotalQuantity existante.
+Pour le prix : On prend la quantité du produit trouvé (dans le panier) et on la multiplie par le prix récupéré dans les données API ! et on remplca ces éléments dans le DOM */
 const getTotalQty = () => {
     let totalQuantity = 0;
     let totalPrice = 0;
 
-    for (let i = 0; i < basketKanaps.length; i++) {
+    for (let i = 0; i < kanapListApi.length; i++) {
+
         for (let j = 0; j < basket.length; j++) {
-            if (basketKanaps[i].id === basket[j].id) {
+            
+            if (kanapListApi[i]._id === basket[j].id) {
                 totalQuantity += basket[j].quantity;
-                totalPrice += basket[j].quantity * basketKanaps[i].price;
+                totalPrice += basket[j].quantity * kanapListApi[i].price;
             }   
         }
+
         document.getElementById("totalQuantity").textContent = totalQuantity;
         document.getElementById("totalPrice").textContent = totalPrice;
     }
@@ -99,17 +107,17 @@ fetch("http://localhost:3000/api/products")
     })
 
     .then((value) => {
-        kanapList = value;
+        kanapListApi = value;
     
         /* Loops the data from the API, and at each iteration a 2nd loop is launched
         Boucle les données de l'API, et à chaque itération une 2ème boucle se lance */
-        for (let i = 0; i < kanapList.length; i++) {
+        for (let i = 0; i < kanapListApi.length; i++) {
             
             /* At each iteration of the first loop, we look for a match in the localStorage
             A chaque itération de la première boucle, on recherche une correspondance dans le localStorage */
             for (let j = 0; j < basket.length; j++) {
 
-                if (kanapList[i]._id === basket[j].id) {
+                if (kanapListApi[i]._id === basket[j].id) {
 
                     /* If a match has been found, the "basketKanap" object is added to the "basketKanaps" array
                     Si une correspondance a été trouvé on ajoute l'objet "basketKanap" dans le tableau "basketKanaps" */
@@ -117,10 +125,10 @@ fetch("http://localhost:3000/api/products")
                         id: basket[j].id,
                         color: basket[j].color,
                         quantity: basket[j].quantity,
-                        name: kanapList[i].name,
-                        price: kanapList[i].price,
-                        img: kanapList[i].imageUrl,
-                        altTxt: kanapList[i].altTxt,
+                        name: kanapListApi[i].name,
+                        price: kanapListApi[i].price,
+                        img: kanapListApi[i].imageUrl,
+                        altTxt: kanapListApi[i].altTxt,
                     };
                     basketKanaps.push(basketKanap);
                 }
